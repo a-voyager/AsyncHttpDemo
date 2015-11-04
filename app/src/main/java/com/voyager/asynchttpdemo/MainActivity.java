86055a13ecd23f8bcfa6539ab29c581de26952fe
wuhaojie <w19961009@126.com>
2015-11-04 23:59:16 +0800

    add download.

--- app/src/main/java/com/voyager/asynchttpdemo/MainActivity.java
+++ app/src/main/java/com/voyager/asynchttpdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.voyager.asynchttpdemo;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -8,19 +9,27 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.apache.http.Header;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btn_start;
+    private Button btn_download;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_start = (Button) findViewById(R.id.btn_start);
+        btn_download = (Button) findViewById(R.id.btn_download);
         btn_start.setOnClickListener(this);
     }
 
@@ -30,7 +39,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_start:
                 opHttp();
                 break;
+            case R.id.btn_download:
+                download();
+                break;
         }
+    }
+
+    private void download() {
+        RequestParams requestParams = new RequestParams();
+        Client.get("http://192.168.1.112:8080/test.avi", requestParams, new FileAsyncHttpResponseHandler(new File(Environment.getExternalStorageDirectory() + "/Download/test.avi")) {
+            @Override
+            public void onFailure(int i, Header[] headers, Throwable throwable, File file) {
+                Toast.makeText(MainActivity.this, "访问失败!", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onSuccess(int i, Header[] headers, File file) {
+                Toast.makeText(MainActivity.this, "访问成功!", Toast.LENGTH_SHORT).show();
+                try {
+                    FileInputStream fis = new FileInputStream(file);
+                    File saveFile = new File(Environment.getExternalStorageDirectory() + "/Download");
+                    if (!saveFile.exists()) {
+                        saveFile.mkdirs();
+                    }
+                    FileOutputStream fos = new FileOutputStream(saveFile);
+//                    while(fis.read())
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
     }
 
     private void opHttp() {
@@ -44,7 +84,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 for (Header header : headers) {
                     Log.d("MAIN", header.toString());
                 }
-                for (byte b :bytes){
+                for (byte b : bytes) {
                     Log.d("MAIN", String.valueOf(b));
                 }
             }
@@ -55,7 +95,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.d("MAIN", "访问失败");
 //                Log.d("MAIN", bytes.toString());
             }
-
 
 
         });
